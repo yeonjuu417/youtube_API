19ea8399def52db2bc8214d481414216e6a1a8ae
Lee Hoyong <igotoweb@gmail.com>
2020-10-05 19:29:44 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { YOUTUBE_API_KEY } from '../config/youtube';
 
-const title = 'hello world';
+const title = '여기에 App 컴포넌트를 렌더링하세요';
 console.log(App);
 console.log(YOUTUBE_API_KEY);
 
